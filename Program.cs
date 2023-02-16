370ff4f831d3f537df1319f58187fbb69ff557df
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-02-16 02:21:16 -0300

    Projeto conta bankaria OK

--- Program.cs
+++ Program.cs
@@ -4,9 +4,119 @@ namespace Dio.Bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
             string opcaoUsuario = ObterOppcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        Transferir();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                        Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                
+                opcaoUsuario = ObterOppcaoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos servicos.");
+            Console.ReadLine();
+        }
+
+        private static void Transferir()
+        {
+            System.Console.WriteLine("Digite o numero da conta de origem");
+            int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o numero da conta de destino");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o valor que voce quer transferir");
+            double valorTransferencia = double.Parse(Console.ReadLine());
+
+            listContas[indiceContaOrigem].Transferir(valorTransferencia, listContas[indiceContaDestino]);
+        }
+
+        private static void Depositar()
+        {
+            System.Console.WriteLine("Digite o numero da conta que ira receber: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorDeposito);
+        }
+
+        private static void Sacar()
+        {
+            System.Console.WriteLine("Digite o numero da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorSaque);
+        }
+
+        private static void ListarContas()
+        {
+            System.Console.WriteLine("Listar contas");
+
+            if(listContas.Count == 0)
+            {
+                System.Console.WriteLine("Nenhuma conta cadastrada");
+                return;
+            }
+            for(int i = 0; i < listContas.Count; i++)
+            {
+                Conta conta = listContas[i];
+                System.Console.Write("#{0} - ", i);
+                System.Console.WriteLine(conta);
+            }
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
+
+            System.Console.WriteLine("Digite 1 para Conta Fisica ou 2 para Juridica");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o Nome do Cliente");
+            string entradaNome = Console.ReadLine();
+
+            System.Console.WriteLine("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            System.Console.WriteLine("Digite o credito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+            
+            listContas.Add(novaConta);
         }
 
         private static string ObterOppcaoUsuario()
@@ -17,7 +127,6 @@ namespace Dio.Bank
             
             Console.WriteLine("1- Listar contas");
             Console.WriteLine("2- Inserir nova conta");
-            Console.WriteLine("3- Transferir");
             Console.WriteLine("3- Transferir");
             Console.WriteLine("4- Sacar");
             Console.WriteLine("5- Depositar");
